a5acb237b4cf9dcf0e60e659aa86ab236fba2170
Michal Firek <mfirek@student.agh.edu.pl>
2025-10-06 22:45:25 +0200

    dokonczenie lab 1 w domu

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,18 +1,23 @@
 package Lab1;
 
-import java.util.Random;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Lotto {
     public static void main(String[] args) {
-        Random rand = new Random();
-        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        Set<Integer> liczby = new HashSet<>();
 
-        while (numbers.size() < 6)
-        {int candidate = rand.nextInt(49) + 1;
-           set.add()
+        while (liczby.size() < 6) {
+            int liczba = random.nextInt(49) + 1;
+            liczby.add(liczba);
         }
-        System.out.println();
+
+        System.out.println("Wylosowane liczby Lotto:");
+        for (int liczba : liczby) {
+            System.out.print(liczba + " ");
+        }
     }
 }
+
