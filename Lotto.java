a355e171753db7a8101cc9c507341584d6f58305
michal firek <mfirek@student.agh.edu.pl>
2025-10-06 16:12:35 +0200

    lotto

---
+++ Lotto.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lotto {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size() < 6)
+        {int candidate = rand.nextInt(49) + 1;
+           set.add()
+        }
+        System.out.println();
+    }
+}
