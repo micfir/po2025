9264847b51028c4eed75e365f65f7d8e2f9ab6d4
psobczy <psobczy@student.agh.edu.pl>
2025-10-27 15:35:31 +0100

    pakiet animal

---
+++ Lab4/src/animals/Animal.java
@@ -0,0 +1,14 @@
+package animals;
+
+public abstract class Animal {
+    String name;
+    int legs;
+    public Animal(String name, int legs) {
+        this.name = name;
+        this.legs = legs;
+    }
+    public Animal() {
+
+    }
+    public abstract String getDescription();
+}
